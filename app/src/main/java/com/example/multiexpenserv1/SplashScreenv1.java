d6a6cd544981f966ca4e063eb90904567e3d5e45
YASH <yashdabhade215@gmail.com>
2021-05-08 11:30:02 +0530

    Created Splash screen and added launcher icon

---
+++ app/src/main/java/com/example/multiexpenserv1/SplashScreenv1.java
@@ -0,0 +1,24 @@
+package com.example.multiexpenserv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenv1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screenv1);
+        new java. util. Timer(). schedule(
+                new java. util. TimerTask() {
+                    @Override
+                    public void run() {
+                    startActivity(new Intent(SplashScreenv1.this,MainActivity.class));
+                    finish();
+                    }
+                },
+                2000);
+    }
+}
