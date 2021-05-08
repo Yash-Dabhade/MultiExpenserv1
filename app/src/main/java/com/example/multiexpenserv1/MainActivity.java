24e2eebb07adb2dcbe0a90731e20783b4b3b0ff8
YASH <yashdabhade215@gmail.com>
2021-05-08 11:48:10 +0530

    Added Backed for starter page and imported assets to the project directory

--- app/src/main/java/com/example/multiexpenserv1/MainActivity.java
+++ app/src/main/java/com/example/multiexpenserv1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.multiexpenserv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Cheking for Onbard process
+        SharedPreferences sharedPreferences=getSharedPreferences("PREFERENCE",MODE_PRIVATE);
+        String FirstTime=sharedPreferences.getString("FirstTimeInstalled","");
+
+        if(FirstTime.equals("Yes")) {
+            startActivity(new Intent(MainActivity.this,Home.class));
+        }
+        else{
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putString("FirstTimeInstalled","Yes");
+            editor.apply();
+        }
     }
 }
