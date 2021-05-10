0498c3eca2e9d5a988a146b942ae66b1d2c42a03
YASH <yashdabhade215@gmail.com>
2021-05-10 15:10:02 +0530

    Successfully Completed the Onboard pages with flexiblity in the layout

--- app/src/main/java/com/example/multiexpenserv1/MainActivity.java
+++ app/src/main/java/com/example/multiexpenserv1/MainActivity.java
@@ -24,27 +24,24 @@ public class MainActivity extends AppCompatActivity {
         if(FirstTime.equals("Yes")) {
             startActivity(new Intent(MainActivity.this,Home.class));
         }
-        else{
-            FirstName=findViewById(R.id.First_Name);
-            LastName=findViewById(R.id.Last_Name);
-            Email=findViewById(R.id.Email_Address);
-            next_button=findViewById(R.id.Next_Button_GS1);
+        else {
+            FirstName = findViewById(R.id.First_Name);
+            LastName = findViewById(R.id.Last_Name);
+            Email = findViewById(R.id.Email_Address);
+            next_button = findViewById(R.id.Next_Button_GS1);
             next_button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    SharedPreferences.Editor editor=sharedPreferences.edit();
-                    editor.putString("FirstName",FirstName.toString());
-                    editor.putString("LastName",LastName.toString());
-                    editor.putString("Email",Email.toString());
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("FirstName", FirstName.toString());
+                    editor.putString("LastName", LastName.toString());
+                    editor.putString("Email", Email.toString());
                     editor.apply();
-                    startActivity(new Intent(MainActivity.this,GettingStarted_two.class));
+                    startActivity(new Intent(MainActivity.this, GettingStarted_two.class));
                 }
             });
 
 
         }
-
-
-
     }
 }
