161f1bef01565c8c233cd7c409f67e173808ef39
Yash Dabhade <yashdabhade215@gmail.com>
2021-05-20 12:33:14 +0530

    Completed Show Expenses in Detail
    Completed show expenses in detail activity with front end and also back end , also added some fonts in the recycler view and also  improved the presentation.

--- app/src/main/java/com/example/multiexpenserv1/Show_Expenses_In_Details.java
+++ app/src/main/java/com/example/multiexpenserv1/Show_Expenses_In_Details.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Show_Expenses_In_Details extends AppCompatActivity {
@@ -24,12 +26,21 @@ public class Show_Expenses_In_Details extends AppCompatActivity {
         TextView amount = findViewById(R.id.Amount_show_expsense_in_details);
         TextView date = findViewById(R.id.Date_show_Expenses_in_details);
         TextView desc=findViewById(R.id.Description_show_expenses_in_detail);
+        ImageView back=findViewById(R.id.back_Show_expenses_in_detail);
 
         //Exporting Data
         title.setText(Title);
         amount.setText(Amount);
         date.setText(Date);
         desc.setText(Description);
+
+        //Setting on click listner for back
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
